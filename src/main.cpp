d600f678eed3cd25c9b23231842f8126458458f8
EliasErhardt7 <e11929888@student.tuwien.ac.at>
2024-12-18 09:27:41 +0100

    Upload data

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 #include <format>
 //#include <fmt/core.h>
 #pragma comment (lib, "d3d11.lib")
-#pragma comment (lib, "DirectXTK.lib")
 #pragma comment (lib, "d3dcompiler.lib")
 
 #include "Dataset.h"
